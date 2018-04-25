67610c6d0e68917533a6abc3caab28991e99346a
soartex34 <texspitt@gmail.com>
2018-04-25 15:09:21 +0200

    first

--- boucle.php
+++ boucle.php
@@ -6,7 +6,24 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body>
+    <body>
 
-</body>
+        <h2>while</h2>
+
+            <?php
+                $nombre_de_lignes = 1;
+
+                while ($nombre_de_lignes <= 10){
+                    echo 'Ceci est la ligne n°' . $nombre_de_lignes .' de la boucle while'.'<br />';
+                    $nombre_de_lignes++;
+                }       
+            ?>
+        <h2>for</h2>
+
+            <?php
+                for ($nombre_de_lignes = 1; $nombre_de_lignes <= 10; $nombre_de_lignes++){
+                    echo 'Ceci est la ligne n°' . $nombre_de_lignes .' de la boucle for'.'<br />';
+                }
+            ?>
+    </body>
 </html>
